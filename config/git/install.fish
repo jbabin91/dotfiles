05ee119157f8f6d220e03ca7bf7c69755e71623b
Jace Babin <jbabin91@gmail.com>
2021-05-28 17:17:23 -0700

    A few updates and added git abbrs

---
+++ config/git/install.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+if command -qa exa
+  abbr -a gl 'git pull --prune'
+  abbr -a glg 'git log --graph --decorate --oneline --abbrev-commit'
+  abbr -a glga 'glg --all'
+  abbr -a gp 'git push origin HEAD'
+  abbr -a gpa 'git push origin --all'
+  abbr -a gd 'git diff'
+  abbr -a gc 'git commit -s'
+  abbr -a gca 'git commit -s -a'
+  abbr -a gco 'git checkout'
+  abbr -a gb 'git branch -v'
+  abbr -a ga 'git add'
+  abbr -a gaa 'git add -A'
+  abbr -a gcm 'git commit -am'
+  abbr -a gs 'git status -sb'
+end
