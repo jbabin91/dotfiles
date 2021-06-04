290c64d35195fc08054ea328f695820c4569e65b
Jace Babin <jbabin91@gmail.com>
2021-06-03 22:35:11 -0700

    Updated the wttr link

--- config/system/functions/wttr.fish
+++ config/system/functions/wttr.fish
@@ -1,8 +1,8 @@
 function wttr -d "Prints out the weather"
 	switch $argv[1]
 	case ''
-		curl https://wttr.in/phoenix
+		curl -s https://wttr.in/phoenix | awk 'NR==2,NR==7'
 	case '*'
-		curl https://wttr.in/$argv[1]
+		curl https://wttr.in/$argv[1] | awk 'NR==2,NR==7'
 	end
 end
