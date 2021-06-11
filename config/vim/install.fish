c478b15de1544689149e12fb3224edbb3bfd08cd
Jace Babin <jbabin91@gmail.com>
2021-06-10 18:43:14 -0700

    Added nvim settings

--- config/vim/install.fish
+++ config/vim/install.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
-curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugUpdate | PlugInstall --sync' +qa
+if ! test ~/.vim/autoload/plug.vim
+  curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  vim +'PlugUpdate | PlugInstall --sync' +qa
+end
