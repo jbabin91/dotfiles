48b4944e434f1539a170edcb89f0e61fce6a0613
Jace Babin <jbabin91@gmail.com>
2021-08-16 16:30:12 -0700

    Updated fish aliases

--- config/exa/install.fish
+++ config/exa/install.fish
@@ -1,11 +1,13 @@
 #!/usr/bin/env fish
+# Changin "ls" to "exa"
 if command -qa exa
-	abbr -a ls 'exa'
-	abbr -a lg 'exa --git'
-	abbr -a l 'exa -lah'
-	abbr -a la 'exa -a'
-	abbr -a ll 'exa -l'
-	abbr -a lt 'exa -lT'
+	abbr -a ls 'exa --color=always --group-directories-first' # my preferred listing
+	abbr -a lg 'exa --git --color=always --group-directories-first'
+	abbr -a l 'exa -lah --color=always --group-directories-first'
+	abbr -a la 'exa -a --color=always --group-directories-first' # all files and dirs
+	abbr -a ll 'exa -l --color=always --group-directories-first' # long format
+	abbr -a lt 'exa -aT --color=always --group-directories-first' # tree listing
+  abbr -a l. 'exa -a1 $* | grep "^\."'
 else
 	abbr -a l 'ls -lAh'
 	abbr -a la 'ls -A'
---
+++ config/fish/aliases/install.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+# Quick edits
+# alias ea 'nvim ~/.config/fish/aliases.fish'
+# alias ef 'nvim ~/.config/fish/config.fish'
+# alias eg 'nvim ~/.gitconfig'
+# alias ev 'nvim ~/.config/nvim/init.vim'
+# alias evv 'vim ~/.vimrc'
+# alias es 'nvim ~/bin/autosort'
+# alias et 'nvim ~/.tmux.conf'
+
+# alias vim-norc 'vim -u NORC'
+# alias vim-none 'vim -u NONE'
+# alias nvim-norc 'nvim -u NORC'
+# alias nvim-none 'nvim -u NONE'
+
+alias g git
+alias t tree
+alias vv 'command vim'
+alias v nvim
+alias vim nvim
+
+alias mx 'chmod -R +x ~/.dotfiles/config'
